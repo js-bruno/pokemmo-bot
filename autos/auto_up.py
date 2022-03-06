de6aa0c3d3289186a4cf2719b51da3da378f1028
js-bruno <brunocebr@hotmail.com>
2022-03-06 15:51:00 -0300

    feat: adding super_effetive_move priority

--- autos/auto_up.py
+++ autos/auto_up.py
@@ -1,14 +1,21 @@
+from genericpath import exists
 from random import choice
 from time import sleep
 
 import pyautogui
 from PIL import Image
 
-
-class Auto_UP:
+# 1 - x=278, y=380
+# 2 - x=531, y=483
+# 3 - x=304, y=540
+# 4 - x=529, y=539
+class Auto_UP:  
+    # TODO: PP CHECK
+    # TODO: HP CHECK
+    # TODO: NO EFFETIVE CHECK
     def auto_up(self):
-        print(pyautogui.getAllTitles())
-        pyautogui.getWindowsWithTitle("РokеММO")[0].activate()
+        pyautogui.getWindowsWithTitle(title="РokeММO")[0].activate()
+        sleep(2)
         while 1 == 1:
             self._battle_mode()
             self._random_moviments()
@@ -18,7 +25,9 @@ class Auto_UP:
         while in_battle != None:
             self._default_battle_position()
             sleep(1)
-            self._battle_move(range(1, 4))
+            exists_super_effetive= self._super_effetive_move()
+            if not exists_super_effetive:
+                self._battle_move([1,2,3,4])
             in_battle = pyautogui.locateOnScreen(
                 Image.open("battleon.png"), confidence=0.9
             )
@@ -30,7 +39,7 @@ class Auto_UP:
         pyautogui.keyUp(choice(moviment))
         pyautogui.press(moviment)
 
-    def _battle_move(self, random_move: range):
+    def _battle_move(self, random_move: list):
         move = choice(random_move)
         if move == 1:
             pyautogui.press(["a", "w"])
@@ -47,3 +56,11 @@ class Auto_UP:
         pyautogui.press(["a", "w"])
         pyautogui.press(["a", "w"])
         pyautogui.press("z")
+
+    def _super_effetive_move(self) -> bool:
+        super_effective_move = pyautogui.locateOnScreen(Image.open("supereffetive.png"), confidence=0.9)
+        if super_effective_move != None:
+            left_top_move = [super_effective_move.left, super_effective_move.top]
+            pyautogui.click(left_top_move)
+            return True
+        return False
